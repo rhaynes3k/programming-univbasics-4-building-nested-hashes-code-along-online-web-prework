9e271650df9ea43e2d9c586f459c903b35152a13
Rommel Haynes <rhaynes3k@gmail.com>
2019-10-19 22:53:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        :name => "Lord Montague", :age => "53"
+        :name => "Lady Montague", :age => "54"
         },
       :hero => {
         :name => "Lord Montague", :age => "53"
