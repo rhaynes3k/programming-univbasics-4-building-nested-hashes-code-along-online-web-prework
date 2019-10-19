d2c833272752f68a52239f6b16a7c2c06804d1b6
Rommel Haynes <rhaynes3k@gmail.com>
2019-10-19 23:01:49 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -25,7 +25,7 @@ def fourth_challenge
         {
           :name => "Steven",
           :age => "30",
-          :attitude => "worried"
+          :attitude => "confused"
         },
         {
           :name => "Mercutio",
